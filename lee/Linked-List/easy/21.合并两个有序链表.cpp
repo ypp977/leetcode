65976bd400cb68bced84e625a04bced29cc0f7cf
ljx <ljxplorer@163.com>
2025-03-18 23:59:09 +0800

    20250318 ljx modify 21.

--- lee/Linked-List/easy/21.合并两个有序链表.cpp
+++ lee/Linked-List/easy/21.合并两个有序链表.cpp
@@ -18,10 +18,28 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        // （1）用循环，归并的思路
+        // ListNode *head = new ListNode(888);
+        // ListNode *cur = head;
+        // while (l1 && l2) {
+        //     if (l1->val <= l2->val) {
+        //         cur->next = l1;
+        //         l1 = l1->next;
+        //     } else {
+        //         cur->next = l2;
+        //         l2 = l2->next;
+        //     }
+        //     cur = cur->next;
+        // }
+        // if (l1) cur->next = l1;
+        // else if (l2) cur->next = l2;
+        // return head->next;
+
+        // （2）递归
         if (!l1) return l2;
         if (!l2) return l1;
         while (l1 && l2) {
-            if (l1->val < l2->val) {
+            if (l1->val <= l2->val) {
                 l1->next = mergeTwoLists(l1->next, l2);
                 return l1;
             } else {
