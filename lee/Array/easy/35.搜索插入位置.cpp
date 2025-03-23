1aaf81d07f729f193ede92124551b38bcabdad28
ljx <borges0728@foxmail.com>
2025-03-23 15:21:49 +0800

    modify 35

--- lee/Array/easy/35.搜索插入位置.cpp
+++ lee/Array/easy/35.搜索插入位置.cpp
@@ -8,13 +8,17 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
-        while (l < r) {
-            int mid = l + r >> 1;
-            if (nums[mid] < target) l = mid + 1;
-            else r = mid;
-        }
-        if (nums[l] >= target) return l; // 返回第一个>=target的元素位置
-        else return l + 1;
+        // （1）STL-lower_bound()
+        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        
+        // （2）二分
+        // int l = 0, r = nums.size() - 1;
+        // while (l < r) {
+        //     int mid = l + r >> 1;
+        //     if (nums[mid] < target) l = mid + 1;
+        //     else r = mid;
+        // }
+        // if (nums[l] >= target) return l; // 返回第一个>=target的元素位置
+        // else return l + 1;
     }
 };
