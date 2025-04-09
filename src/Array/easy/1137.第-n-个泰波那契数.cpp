82b913cebebdca618dfeea71eded3bd8172e26dc
ypp977 <1808419119@qq.com>
2025-04-09 13:21:19 +0800

    20250409

---
+++ src/Array/easy/1137.第-n-个泰波那契数.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1137 lang=cpp
+ *
+ * [1137] 第 N 个泰波那契数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int tribonacci(int n) {
+        
+    }
+};
+// @lc code=end
+
