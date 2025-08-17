86eec5f2938bcbb8816e3191112e36525af71500
ypp977 <1808419119@qq.com>
2025-08-18 00:11:37 +0800

    20250818

---
+++ src/Binary-Search/easy/222.完全二叉树的节点个数.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=cpp
+ *
+ * [222] 完全二叉树的节点个数
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(!root)
+        {
+            return 0;
+        }
+
+        int rh = 0, lh = 0;
+        TreeNode *l = root, *r = root;
+        while(l)
+        {
+            ++lh;
+            l = l->left;
+        }
+        while(r)
+        {
+            ++rh;
+            r = r->right;
+        }
+
+        if(lh == rh)
+        {
+            return (1 << lh) - 1;
+        }
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
+// @lc code=end
+
