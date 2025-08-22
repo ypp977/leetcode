54289aeb4f9346bf98822242a360f25a86ec9ff4
ypp977 <1808419119@qq.com>
2025-08-22 23:57:53 +0800

    20250822

---
+++ src/Binary-Search/easy/392.判断子序列.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=392 lang=cpp
+ *
+ * [392] 判断子序列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.size(), n = t.size();
+        int i = 0, j = 0;
+        while (i < m && j < n)
+        {
+            if(s[i] == t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+        return i == m;
+    }
+};
+// @lc code=end
+
