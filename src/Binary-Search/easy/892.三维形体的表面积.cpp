8eef1a0d3dedfc4c98d62f25361372020e7f0502
ypp977 <1808419119@qq.com>
2025-12-14 19:45:15 +0800

    20251214

---
+++ src/Binary-Search/easy/892.三维形体的表面积.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=892 lang=cpp
+ *
+ * [892] 三维形体的表面积
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int surfaceArea(vector<vector<int>>& grid) {
+        class Solution
+        {
+        public:
+            int surfaceArea(vector<vector<int>> &grid)
+            {
+                int res = 0;
+                for (int i = 0; i < grid.size(); i++)
+                {
+                    for (int j = 0; j < grid[0].size(); j++)
+                    {
+                        if (grid[i][j])
+                        {
+                            res += 2;                               // 1 for up, 1 for down
+                            res += fun(grid, i - 1, j, grid[i][j]); // left
+                            res += fun(grid, i, j - 1, grid[i][j]); // front
+                            res += fun(grid, i + 1, j, grid[i][j]); // right
+                            res += fun(grid, i, j + 1, grid[i][j]); // back
+                        }
+                    }
+                }
+                return res;
+            }
+            int fun(vector<vector<int>> &grid, int i, int j, int val)
+            {
+                if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
+                    return val; // if no adjacent cube then total area is contributed
+
+                if (grid[i][j] < val) // if neighbour cube is there then the diff is contributed
+                    return val - grid[i][j];
+
+                return 0; // if the current is smaller than its neighbour, no contribution
+            }
+        };
+    }
+};
+// @lc code=end
+
