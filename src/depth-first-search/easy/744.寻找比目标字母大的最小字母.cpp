329262d053e35b52a5a09d99782c896ac4e342cd
ypp977 <1808419119@qq.com>
2026-02-06 19:14:27 +0800

    20260206

---
+++ src/depth-first-search/easy/744.寻找比目标字母大的最小字母.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=744 lang=cpp
+ *
+ * [744] 寻找比目标字母大的最小字母
+ */
+
+// @lc code=start
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int low = 0, high = letters.size();
+        while(low < high)
+        {
+            int mid = low + (high - low) / 2;
+            if(letters[mid] <= target)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return letters[low % letters.size()];
+    }
+};
+// @lc code=end
+
