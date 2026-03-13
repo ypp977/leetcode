2d21fe06c99a98ee7d1b224f00711372d2db34e3
ypp977 <1808419119@qq.com>
2026-03-13 19:23:08 +0800

    20260313

---
+++ src/stack/easy/232.用栈实现队列.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=232 lang=cpp
+ *
+ * [232] 用栈实现队列
+ */
+
+// @lc code=start
+class Queue
+{
+    stack<int> input, output;
+
+public:
+    void push(int x)
+    {
+        input.push(x);
+    }
+
+    void pop(void)
+    {
+        peek();
+        output.pop();
+    }
+
+    int peek(void)
+    {
+        if (output.empty())
+            while (input.size())
+                output.push(input.top()), input.pop();
+        return output.top();
+    }
+
+    bool empty(void)
+    {
+        return input.empty() && output.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+// @lc code=end
+
